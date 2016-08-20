b93165afacd4dbdf8bdbfb40e587530489ec8904
Raev82 <powell.richard.a@gmail.com>
2016-08-20 15:14:11 -0400

    Lets Mod Reboot with Pahimar
    First Upload

--- src/main/java/com/raev82/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/raev82/letsmodreboot/LetsModReboot.java
@@ -3,6 +3,7 @@ package com.raev82.letsmodreboot;
 import com.raev82.letsmodreboot.handler.ConfigurationHandler;
 import com.raev82.letsmodreboot.init.ModBlocks;
 import com.raev82.letsmodreboot.init.ModItems;
+import com.raev82.letsmodreboot.init.Recipes;
 import com.raev82.letsmodreboot.proxy.IProxy;
 import com.raev82.letsmodreboot.reference.Reference;
 import com.raev82.letsmodreboot.utility.LogHelper;
@@ -12,6 +13,7 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.oredict.OreDictionary;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class LetsModReboot
@@ -39,11 +41,22 @@ public class LetsModReboot
     public void init(FMLInitializationEvent event)
     {
         LogHelper.info("Initialization Complete!");
+        Recipes.init();
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
         LogHelper.info("Post Initialization Complete!");
+
+        /*
+        Use for debugging only.  Lists out all items added to the Ore Dictionary.
+
+        for (String oreName : OreDictionary.getOreNames())
+        {
+            LogHelper.info(oreName);
+            OreDictionary.getOres(oreName);
+        }
+        */
     }
 }
